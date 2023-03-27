8104cddd2528082386172ad815080e1be6be2a9a
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-27 13:15:49 +0300

    highscore functionality

--- 05_guess_my_number/script.js
+++ 05_guess_my_number/script.js
@@ -10,6 +10,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -25,6 +26,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
   }
   // When player guess greater number
   else if (guess > secretNumber) {
