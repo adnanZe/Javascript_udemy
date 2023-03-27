e2242ae2cddb88d5605ba2b3e183d94e44171e23
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-27 14:09:56 +0300

    destructuring arrays

--- data_structure_operators/script.js
+++ data_structure_operators/script.js
@@ -26,4 +26,23 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+const [first, , second] = restaurant.categories;
+console.log(first, second);
+
+const [starter, main] = restaurant.order(2, 0);
+console.log(starter, main);
