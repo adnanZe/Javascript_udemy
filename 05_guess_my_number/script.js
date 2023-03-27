d6b1fd3da3123ab21acf246f4b2aa70c37b57825
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-27 13:29:05 +0300

    small DRY

--- 05_guess_my_number/script.js
+++ 05_guess_my_number/script.js
@@ -12,47 +12,41 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   // When there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔ No number!";
+    displayMessage("⛔ No number!");
   }
 
   // When player wins
   else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "🎉 Correct Number!";
-    document.querySelector(".number").textContent = secretNumber;
-    document.querySelector("body").style.backgroundColor = "#60b347";
-    document.querySelector(".number").style.width = "30rem";
+    if (score > 1) {
+      displayMessage("🎉 Correct Number!");
+      document.querySelector(".number").textContent = secretNumber;
+      document.querySelector("body").style.backgroundColor = "#60b347";
+      document.querySelector(".number").style.width = "30rem";
+    }
 
     if (score > highScore) {
       highScore = score;
       document.querySelector(".highscore").textContent = highScore;
     }
   }
-  // When player guess greater number
-  else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "📈 Too high!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "💥 You lost the game!";
-      document.querySelector(".score").textContent = 0;
-    }
-  }
-  // When player guess lowest number
-  else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "📉 Too low!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "💥 You lost the game!";
-      document.querySelector(".score").textContent = 0;
-    }
+
+  // When is wrong
+  else if (guess !== secretNumber) {
+    displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+
+    score--;
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".message").textContent = "💥 You lost the game!";
+    document.querySelector(".score").textContent = 0;
   }
 });
 
