247c12562b8e21d32b5d9e4bd0bdbca3ad94c5ea
AdnanZe <adnan.zecheria@yahoo.com>
2023-03-27 10:51:49 +0300

    working with textContent

--- 05_guess_my_number/script.js
+++ 05_guess_my_number/script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+document.querySelector(".message").textContent = "🎉 Correct Number!";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
